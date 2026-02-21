a7a13674c458cbec307e0a5f6b4487186b4269bd
CVHS_Software_Falkons <cvhsfrc589@gmail.com>
2026-02-20 16:07:45 -0800

    got can range working

--- src/main/java/frc/robot/subsystems/TelemetrySubsystem.java
+++ src/main/java/frc/robot/subsystems/TelemetrySubsystem.java
@@ -4,25 +4,37 @@
 
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Centimeter;
+import static edu.wpi.first.units.Units.Inches;
+
 import com.ctre.phoenix6.configs.CANrangeConfiguration;
 import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.signals.UpdateModeValue;
 
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TelemetrySubsystem extends SubsystemBase {
   private static PowerDistribution m_PDH = new PowerDistribution();
+
   private final CANrange m_canRange = new CANrange(61);
+
   /** Creates a new TelemetrySubsystem. */
   public TelemetrySubsystem() {
     CANrangeConfiguration config = new CANrangeConfiguration();
 
-    config.ProximityParams.MinSignalStrengthForValidMeasurement = 2000; // If CANrange has a signal strength of at least 2000, it is a valid measurement.
-    config.ProximityParams.ProximityThreshold = 0.1; // If CANrange detects an object within 0.1 meters, it will trigger the "isDetected" signal.
+    config.ProximityParams.MinSignalStrengthForValidMeasurement = 2000; // If
+    // CANrange has a signal strength of at least
+    // 2000, it is a valid measurement.
+    config.ProximityParams.ProximityThreshold = 0.15; // If CANrange detects an
+    // object within 0.1 meters, it will trigger
+    // the "isDetected" signal.
 
-    config.ToFParams.UpdateMode = UpdateModeValue.ShortRange100Hz; // Make the CANrange update as fast as possible at 100 Hz. This requires short-range mode.
+    config.ToFParams.UpdateMode = UpdateModeValue.ShortRange100Hz; // Make the
+    // CANrange update as fast as possible at
+    // 100 Hz. This requires short-range mode.
 
     m_canRange.getConfigurator().apply(config);
   }
@@ -33,9 +45,12 @@ public class TelemetrySubsystem extends SubsystemBase {
 
     SmartDashboard.putNumber("Robot Voltage", m_PDH.getVoltage());
     SmartDashboard.putNumber("PDH Port 20 Current", m_PDH.getCurrent(20));
-    SmartDashboard.putNumber("Range Finder Distance", getCANRangeDistance());
+    SmartDashboard.putNumber("Range Finder Distance", getCANRangeDistance().in(Centimeter));
+    SmartDashboard.putNumber("Range Finder Signal Strength", m_canRange.getSignalStrength().getValueAsDouble());
+    SmartDashboard.putBoolean("Range Finder IsDetected", m_canRange.getIsDetected().getValue());
   }
-  public double getCANRangeDistance(){
-    return m_canRange.getDistance().getValueAsDouble();
+
+  public Distance getCANRangeDistance() {
+    return m_canRange.getDistance().getValue();
   }
 }
